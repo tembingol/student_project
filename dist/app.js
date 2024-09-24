946a2079b1a8481e4392c8948c7554db88d8aa29
Alexandr Rastorguyev <tembingol@gmail.com>
2024-09-24 23:43:38 +0500

    add: inplemented two endpoints. 1 - get all videos, 2 - create vireo
    Signed-off-by: Alexandr Rastorguyev <tembingol@gmail.com>

--- dist/app.js
+++ dist/app.js
@@ -5,17 +5,56 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
-//import cors from 'cors'
+const db_1 = require("./db/db");
+//import { getVideosController } from './videos/getVideosController'
 exports.app = (0, express_1.default)(); // создать приложение
 exports.app.use(express_1.default.json()); // создание свойств-объектов body и query во всех реквестах
-//app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
 exports.app.get('/', (req, res) => {
     // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
     res.status(200).json({ version: '1.1' });
 });
 exports.app.get('/videos', (req, res) => {
-    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
-    res.status(200).json({ version: '1.1', videos: "videos" });
+    const myArray = db_1.db.videos;
+    res.status(200).json(myArray);
 });
-// app.get(SETTINGS.PATH.VIDEOS, getVideosController)
+exports.app.post('/videos', (req, res) => {
+    if (!req.body.title || req.body.title.length > 40) {
+        const error = {
+            "errorsMessages": [
+                {
+                    "message": "incorrect values",
+                    "field": "title"
+                }
+            ]
+        };
+        res.status(400).json(error);
+        return;
+    }
+    if (!req.body.author || req.body.author.length > 20) {
+        const error = {
+            "errorsMessages": [
+                {
+                    "message": "incorrect values",
+                    "field": "author"
+                }
+            ]
+        };
+        res.status(400).json(error);
+        return;
+    }
+    const newVideoObject = {
+        "id": db_1.db.videos.length + 1,
+        "title": req.body.title,
+        "author": req.body.author,
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": new Date(),
+        "publicationDate": new Date(),
+        "availableResolutions": req.body.availableResolutions,
+    };
+    db_1.db.videos.push(newVideoObject);
+    const myArray = db_1.db.videos;
+    res.status(200).json(newVideoObject);
+});
+//app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 // app.use(SETTINGS.PATH.VIDEOS, videosRouter)
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,64 @@
 import express from 'express'
-//import cors from 'cors'
- 
+import {db} from './db/db'
+//import { getVideosController } from './videos/getVideosController'
+
 export const app = express() // создать приложение
 app.use(express.json()) // создание свойств-объектов body и query во всех реквестах
-//app.use(cors()) // разрешить любым фронтам делать запросы на наш бэк
  
 app.get('/', (req, res) => {
     // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
     res.status(200).json({version: '1.1'})
 })
 app.get('/videos', (req, res) => {
-    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
-    res.status(200).json({version: '1.1', videos: "videos"})
+    const myArray = db.videos
+    res.status(200).json(myArray)
 })
-// app.get(SETTINGS.PATH.VIDEOS, getVideosController)
+
+app.post('/videos', (req, res) => {
+ 
+ 
+    if(!req.body.title || req.body.title.length>40){
+        const error =
+        {
+            "errorsMessages": [
+              {
+                "message": "incorrect values",
+                "field": "title"
+              }
+            ]
+          }
+          res.status(400).json(error)
+          return
+    }
+
+    if(!req.body.author || req.body.author.length>20){
+        const error ={
+            "errorsMessages": [
+              {
+                "message": "incorrect values",
+                "field": "author"
+              }
+            ]
+          }
+          res.status(400).json(error)
+          return
+    }
+
+    const newVideoObject = {
+        "id": db.videos.length + 1,
+        "title": req.body.title,
+        "author": req.body.author,
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": new Date(),
+        "publicationDate": new Date(),
+        "availableResolutions": req.body.availableResolutions,
+      }
+      db.videos.push(newVideoObject)
+
+    const myArray = db.videos
+    res.status(200).json(newVideoObject)
+})
+
+//app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 // app.use(SETTINGS.PATH.VIDEOS, videosRouter)
