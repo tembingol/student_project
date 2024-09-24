15060553d5029b1ab6a09eed8e1737d934db55f0
Alexandr Rastorguyev <tembingol@gmail.com>
2024-09-25 00:30:18 +0500

    fix: fixed deprecated methot "send(<status>)"
    Signed-off-by: Alexandr Rastorguyev <tembingol@gmail.com>

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -9,70 +11,70 @@ const db_1 = require("./db/db");
 //import { getVideosController } from './videos/getVideosController'
 exports.app = (0, express_1.default)(); // создать приложение
 exports.app.use(express_1.default.json()); // создание свойств-объектов body и query во всех реквестах
-exports.app.get('/', (req, res) => {
-    // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
-    res.status(200).json({ version: '1.1' });
+exports.app.get("/", (req, res) => {
+  // эндпоинт, который будет показывать на верселе какая версия бэкэнда сейчас залита
+  res.status(200).json({ version: "1.1" });
 });
-exports.app.get('/videos', (req, res) => {
-    const myArray = db_1.db.videos;
-    res.status(200).json(myArray);
+exports.app.get("/videos", (req, res) => {
+  const myArray = db_1.db.videos;
+  res.status(200).json(myArray);
 });
-exports.app.get('/videos/:id', (req, res) => {
-    const myArray = db_1.db.videos.filter((e) => +e.id === +req.params.id);
-    if (myArray.length == 0) {
-        res.send(404);
-        return;
-    }
-    res.status(200).json(myArray[0]);
+exports.app.get("/videos/:id", (req, res) => {
+  const myArray = db_1.db.videos.filter((e) => +e.id === +req.params.id);
+  if (myArray.length == 0) {
+    res.sendStatus(404);
+    return;
+  }
+  res.status(200).json(myArray[0]);
 });
-exports.app.delete('/videos/:id', (req, res) => {
-    const myArray = db_1.db.videos.filter((e) => +e.id === +req.params.id);
-    if (myArray.length == 0) {
-        res.send(404);
-        return;
-    }
-    const objectIndex = db_1.db.videos.indexOf(myArray[0]);
-    db_1.db.videos.splice(objectIndex, 1);
-    res.send(204);
+exports.app.delete("/videos/:id", (req, res) => {
+  const myArray = db_1.db.videos.filter((e) => +e.id === +req.params.id);
+  if (myArray.length == 0) {
+    res.sendStatus(404);
+    return;
+  }
+  const objectIndex = db_1.db.videos.indexOf(myArray[0]);
+  db_1.db.videos.splice(objectIndex, 1);
+  res.sendStatus(204);
 });
-exports.app.post('/videos', (req, res) => {
-    if (!req.body.title || req.body.title.length > 40) {
-        const error = {
-            "errorsMessages": [
-                {
-                    "message": "incorrect values",
-                    "field": "title"
-                }
-            ]
-        };
-        res.status(400).json(error);
-        return;
-    }
-    if (!req.body.author || req.body.author.length > 20) {
-        const error = {
-            "errorsMessages": [
-                {
-                    "message": "incorrect values",
-                    "field": "author"
-                }
-            ]
-        };
-        res.status(400).json(error);
-        return;
-    }
-    const newVideoObject = {
-        "id": db_1.db.videos.length + 1,
-        "title": req.body.title,
-        "author": req.body.author,
-        "canBeDownloaded": true,
-        "minAgeRestriction": null,
-        "createdAt": new Date(),
-        "publicationDate": new Date(),
-        "availableResolutions": req.body.availableResolutions,
+exports.app.post("/videos", (req, res) => {
+  if (!req.body.title || req.body.title.length > 40) {
+    const error = {
+      errorsMessages: [
+        {
+          message: "incorrect values",
+          field: "title",
+        },
+      ],
     };
-    db_1.db.videos.push(newVideoObject);
-    const myArray = db_1.db.videos;
-    res.status(200).json(newVideoObject);
+    res.status(400).json(error);
+    return;
+  }
+  if (!req.body.author || req.body.author.length > 20) {
+    const error = {
+      errorsMessages: [
+        {
+          message: "incorrect values",
+          field: "author",
+        },
+      ],
+    };
+    res.status(400).json(error);
+    return;
+  }
+  const newVideoObject = {
+    id: db_1.db.videos.length + 1,
+    title: req.body.title,
+    author: req.body.author,
+    canBeDownloaded: true,
+    minAgeRestriction: null,
+    createdAt: new Date(),
+    publicationDate: new Date(),
+    availableResolutions: req.body.availableResolutions,
+  };
+  db_1.db.videos.push(newVideoObject);
+  const myArray = db_1.db.videos;
+  res.status(200).json(newVideoObject);
 });
 //app.get(SETTINGS.PATH.VIDEOS, getVideosController)
 // app.use(SETTINGS.PATH.VIDEOS, videosRouter)
